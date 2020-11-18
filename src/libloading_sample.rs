09d4bedd0bc7a24118b086a19d40e089b50b385a
Anirudh Sivaraman <sk.anirudh@gmail.com>
2020-11-18 13:02:50 -0500

    Get library name from command line.

--- src/libloading_sample.rs
+++ src/libloading_sample.rs
@@ -1,5 +1,6 @@
+use std::env;
 fn main() {
-  let dyn_lib   = libloading::Library::new("target/debug/libplugin_sample.dylib").expect("load library");
+  let dyn_lib   = libloading::Library::new(&env::args().collect::<Vec<String>>()[1]).expect("load library");
   let loaded_fn : libloading::Symbol<extern fn(u32) -> ()> = unsafe { dyn_lib.get(b"codelet") }.expect("load symbol");
   loaded_fn(53);
 }
