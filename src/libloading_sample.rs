23380093e0aae530b97bf21d8186fdb81f98f9eb
Anirudh Sivaraman <sk.anirudh@gmail.com>
2020-11-22 16:19:54 -0500

    Remove libloading sample program.

--- src/libloading_sample.rs
+++
@@ -1,6 +0,0 @@
-use std::env;
-fn main() {
-  let dyn_lib   = libloading::Library::new(&env::args().collect::<Vec<String>>()[1]).expect("load library");
-  let loaded_fn : libloading::Symbol<extern fn(u32) -> ()> = unsafe { dyn_lib.get(b"codelet") }.expect("load symbol");
-  loaded_fn(53);
-}
