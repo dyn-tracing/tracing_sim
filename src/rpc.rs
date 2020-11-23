996c7e37aaa7e6aaf2068680097ac5077ed1efce
Anirudh Sivaraman <sk.anirudh@gmail.com>
2020-11-23 14:40:45 -0500

    Use counter UID; faster than UIUDs, but still slower than before.

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,13 +1,15 @@
-use uuid::Uuid;
-
 #[derive(PartialEq, Clone, Debug)]
 #[repr(C)]
 pub struct Rpc {
    pub id      : u32,
-   pub uuid    : Uuid,
+   pub uid     : u64,
 }
 
 impl Rpc {
     pub fn get_id(&self) -> u32 { self.id }
-    pub fn new(id : u32) -> Self { Rpc { id : id, uuid : Uuid::new_v4() } }
+    pub fn new(id : u32) -> Self {
+        static mut COUNTER : u64 = 0;
+        unsafe { COUNTER = COUNTER + 1; }
+        unsafe { Rpc { id : id, uid : COUNTER } }
+    }
 }
