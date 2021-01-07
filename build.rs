0a87c8b2a57a83bd6fe23b0e549e92bbfb844c95
jessberg <jessberg@users.noreply.github.com>
2021-01-07 13:45:15 -0500

    Add support for state in filters/plugins (#4)
    * Compiled filter from dyn_compiler works and is in plugins
    
    * Added path field to RPC struct
    
    * Path field of rpc struct now contains the correct trace path
    
    * Added state to filters
    
    * Import the correct library in plugin tests
    
    * All pull request comments except mutability in src/simulator.rs fixed
    
    * Add comments.
    
    Co-authored-by: Anirudh Sivaraman <sk.anirudh@gmail.com>

--- build.rs
+++ build.rs
@@ -2,22 +2,16 @@ use std::process::Command;
 
 // Example custom build script.
 fn main() {
+    // TODO: use handlebars to automatically generate filters, that way you abstract away the code that is used
+    //       for all filters
+
     // Note: needs to be run with cargo -vv to generate output from these commands.
     eprintln!("{:?}",
-              Command::new("rustc").args(&["plugins/sample.rs", "--crate-type=dylib"]).output().unwrap());
-    eprintln!("{:?}",
-              Command::new("rustc").args(&["plugins/sample2.rs", "--crate-type=dylib"]).output().unwrap());
-    eprintln!("{:?}",
-              Command::new("rustc").args(&["plugins/sink.rs", "--crate-type=dylib"]).output().unwrap());
+              Command::new("rustc").args(&["plugins/sample_filter.rs", "--crate-type=dylib"]).output().unwrap());
 
     // Don't go any further if these commands failed.
-    assert!(Command::new("rustc").args(&["plugins/sample.rs",  "--crate-type=dylib"]).status().unwrap().success());
-    assert!(Command::new("rustc").args(&["plugins/sample2.rs", "--crate-type=dylib"]).status().unwrap().success());
-    assert!(Command::new("rustc").args(&["plugins/sink.rs"  ,  "--crate-type=dylib"]).status().unwrap().success());
+    assert!(Command::new("rustc").args(&["plugins/sample_filter.rs",  "--crate-type=dylib"]).status().unwrap().success());
 
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed=./plugins");
-    println!("cargo:rerun-if-changed=./plugins/sample.rs");
-    println!("cargo:rerun-if-changed=./plugins/sample2.rs");
-    println!("cargo:rerun-if-changed=./plugins/sink.rs");
 }
