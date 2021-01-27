1620c006af64101cde7c8423ae324babad93f803
fruffy <5960321+fruffy@users.noreply.github.com>
2021-01-27 16:06:26 -0500

    Add fmt and clippy to simulator (#17)
    * fmt
    
    * Add fmt
    
    * Slightly different approach.
    
    * Different approach.
    
    * Fix conflict.
    
    * Address complaints.
    
    * Syntax changed.
    
    * Missed some complaints.
    
    * Petgraph causes issues with clippy.
    
    * Disable clippy until petgraph is external again

--- libs/rpc_lib/src/rpc.rs
+++ libs/rpc_lib/src/rpc.rs
@@ -3,9 +3,9 @@ use std::collections::HashMap;
 #[derive(PartialEq, Clone, Debug)]
 #[repr(C)]
 pub struct Rpc {
-    pub data: u32,    // application data
-    pub uid: u64,     // number of hops the message has taken
-    pub path: String, // the path that the request has taken thus far
+    pub data: u32,                        // application data
+    pub uid: u64,                         // number of hops the message has taken
+    pub path: String,                     // the path that the request has taken thus far
     pub headers: HashMap<String, String>, // the "http" headers of the rpc, ie, filter-defined book keeping
 }
 
@@ -14,19 +14,19 @@ impl Rpc {
         static mut COUNTER: u64 = 0;
         let ret = unsafe {
             Rpc {
-                data: data,
+                data,
                 uid: COUNTER,
                 path: String::new(),
                 headers: HashMap::new(),
             }
         };
         unsafe {
-            COUNTER = COUNTER + 1;
+            COUNTER += 1;
         }
         ret
     }
     pub fn add_to_path(&mut self, hop: &str) {
-        self.path.push_str(" ");
+        self.path.push(' ');
         self.path.push_str(hop);
     }
 }
