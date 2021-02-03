297673a549663b0574327777b7804b96036c2212
Jessica Berg <jesswberg@gmail.com>
2021-02-03 15:04:10 -0500

    fmt

--- sim/src/lib.rs
+++ sim/src/lib.rs
@@ -1,9 +1,9 @@
 #![feature(test)]
 #![feature(extern_types)]
 mod edge;
-mod node;
-mod sim_element;
-mod storage;
-mod plugin_wrapper;
 mod filter_types;
+mod node;
+mod plugin_wrapper;
+mod sim_element;
 pub mod simulator;
+mod storage;
